976354b2574b81587ef47823bc582d7f6a3d71af
Lorenzo Benedetti <lorenzo.benedetti5@stud.unifi.it>
2023-12-14 22:57:14 +0100

    Added getters

--- src/Booking.java
+++ src/Booking.java
@@ -5,13 +5,15 @@ public class Booking {
     private Hotel hotel;
     private PaymentStrategy strategy;
     private BookingMediator mediator;
+    private String bookingStatus;
 
-    public Booking(Customer customer, Flight flight, Hotel hotel, PaymentStrategy strategy, BookingMediator mediator) {
+    public Booking(Customer customer, Flight flight, Hotel hotel, PaymentStrategy strategy, BookingMediator mediator, String bookingStatus) {
         this.customer = customer;
         this.flight = flight;
         this.hotel = hotel;
         this.strategy = strategy;
         this.mediator = mediator;
+        this.bookingStatus = bookingStatus;
     }
 
     public Customer getCustomer() {
@@ -26,12 +28,36 @@ public class Booking {
         return hotel;
     }
 
+    public PaymentStrategy getStrategy() {
+        return new BookingAdapterImpl(this);
+    }
+
+    public BookingMediator getMediator() {
+        return mediator;
+    }
+
+    public int getTotalPrice() {
+        return flight.getPrice() + hotel.getPrice();
+    }
+
+    public void pay() {
+        strategy.pay(this);
+    }
+
     public void confirmBooking() {
         mediator.bookFlightAndHotel(this);
     }
 
     public void cancelBooking() {
         mediator.cancelBooking(this);
+    }
+
+    public String getBookingStatus() {
+        return bookingStatus;
+    }
+
+    public void setBookingStatus(String bookingStatus) {
+        this.bookingStatus = bookingStatus;
     }
 
 }
