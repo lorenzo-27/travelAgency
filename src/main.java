455c74f6303ae74669a4224df4f026a8cb74b7c3
Lorenzo Benedetti <102320752+lorenzo-27@users.noreply.github.com>
2024-01-19 19:42:01 +0100

    Updated temporary main file.

--- src/main.java
+++ src/main.java
@@ -8,10 +8,10 @@ public class main {
         customer customer = new customer("Mario Rossi", "mario.rossi@example.com", "1234567890");
 
         // Crea un volo
-        flight flight = new flight("ABC123", "Ryanair", "FCO", "CPH", LocalDateTime.of(2023, 11, 20, 10, 00), LocalDateTime.of(2023, 11, 20, 16, 00), 3, 100);
+        flight flight = new flight("ABC123", "Ryanair", "FCO", "CPH", LocalDateTime.of(2023, 11, 20, 10, 00), LocalDateTime.of(2023, 11, 20, 16, 00), 3, 100, 20);
 
         // Crea un hotel
-        hotel hotel = new hotel("ABC123", "Hotel California", "Via Roma 100, Firenze", "Firenze", "Italia", 5);
+        hotel hotel = new hotel("ABC123", "Hotel California", "Via Roma 100, Firenze", "Firenze", "Italia", 5, 2);
 
         // Crea una strategia di pagamento
         paymentStrategy strategy = new payPalStrategy();
@@ -22,7 +22,7 @@ public class main {
 
         // Associa il mediatore all'agenzia di viaggi
         mediator.setTravelAgency(travelAgency);
-        mediator.bookFlightAndHotel(new booking(customer, flight, hotel, strategy, mediator, "pending"));
+        //mediator.bookFlightAndHotel(new booking(customer, flight, hotel, strategy, mediator, "pending"));
 
         // Crea una prenotazione
         booking booking = travelAgency.createBooking(customer, flight, hotel, strategy, "pending");
