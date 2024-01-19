2651fa0543f04fc91711e1cff8b3da9ce4ff8cb3
Lorenzo Benedetti <102320752+lorenzo-27@users.noreply.github.com>
2024-01-19 19:31:39 +0100

    Added booking status updater.

--- src/travelAgency.java
+++ src/travelAgency.java
@@ -14,12 +14,15 @@ public class travelAgency {
     public booking createBooking(customer customer, flight flight, hotel hotel, paymentStrategy strategy, String bookingStatus) {
         booking booking = new booking(customer, flight, hotel, strategy, mediator, bookingStatus);
         this.bookings.add(booking);
+        booking.confirmBooking();
+        booking.setBookingStatus("Confermata");
         return booking;
     }
 
     public void cancelBooking(booking booking) {
         this.bookings.remove(booking);
         booking.cancelBooking();
+        booking.setBookingStatus("Annullata");
     }
 
     public void setMediator(bookingMediator mediator) {
