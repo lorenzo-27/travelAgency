462aa4b5c60c42092f9511aeb4aee0b65c184194
Lorenzo Benedetti <102320752+lorenzo-27@users.noreply.github.com>
2024-01-20 18:23:36 +0100

    Implemented additional useful methods.

--- src/travelAgency.java
+++ src/travelAgency.java
@@ -22,8 +22,60 @@ public class travelAgency {
         mediator.cancelBooking(booking);
     }
 
+
+    // The following methods are used to add some functionality to the travel agency
     public List<booking> getAllBookings() {
         return new ArrayList<>(bookings);
+    }
+
+    public List<booking> getBookingsByCustomer(customer customer) {
+        List<booking> bookingsByCustomer = new ArrayList<>();
+        for (booking booking : bookings) {
+            if (booking.getCustomer().equals(customer)) {
+                bookingsByCustomer.add(booking);
+            }
+        }
+        return bookingsByCustomer;
+    }
+
+    public List<booking> getBookingsByFlight(flight flight) {
+        List<booking> bookingsByFlight = new ArrayList<>();
+        for (booking booking : bookings) {
+            if (booking.getFlight().equals(flight)) {
+                bookingsByFlight.add(booking);
+            }
+        }
+        return bookingsByFlight;
+    }
+
+    public List<booking> getBookingsByHotel(hotel hotel) {
+        List<booking> bookingsByHotel = new ArrayList<>();
+        for (booking booking : bookings) {
+            if (booking.getHotel().equals(hotel)) {
+                bookingsByHotel.add(booking);
+            }
+        }
+        return bookingsByHotel;
+    }
+
+    public List<booking> getBookingsByPaymentStrategy(paymentStrategy strategy) {
+        List<booking> bookingsByPaymentStrategy = new ArrayList<>();
+        for (booking booking : bookings) {
+            if (booking.getStrategy().equals(strategy)) {
+                bookingsByPaymentStrategy.add(booking);
+            }
+        }
+        return bookingsByPaymentStrategy;
+    }
+
+    public List<booking> getBookingsByBookingStatus(String bookingStatus) {
+        List<booking> bookingsByBookingStatus = new ArrayList<>();
+        for (booking booking : bookings) {
+            if (booking.getBookingStatus().equals(bookingStatus)) {
+                bookingsByBookingStatus.add(booking);
+            }
+        }
+        return bookingsByBookingStatus;
     }
 
 }
